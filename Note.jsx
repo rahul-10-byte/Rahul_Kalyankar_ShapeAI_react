56a1e3b090f25e422f451f33a5a6658825182ed4
Rahul Kalyankar <kalyankarrahul500@gmail.com>
2021-06-07 18:03:03 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut eligendi
+        neque eveniet magni possimus, facilis cumque consequuntur iste
+        provident.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
